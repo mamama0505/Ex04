4534411344ed84ba2f1aba134b2c6976d8ab5baf
mamama0505 <109198961+mamama0505@users.noreply.github.com>
2022-07-13 22:26:09 +0900

    HoT: initial revision

---
+++ coin.java
@@ -0,0 +1,22 @@
+
+public class coin {
+ public static void main(String[] args){
+        int result = 0;
+        int omote = 0;
+        int ura = 0;
+        System.out.println("Tossing a coin...");
+        for(int i = 1; i <= 3; i ++){
+        int coin = new java.util.Random().nextInt(2);
+        if(coin == 0){
+        omote++;
+        System.out.println("Round"+i+": Heads");
+        } else if(coin == 1){
+        ura++;
+        System.out.println("Round"+i+": Tails");
+        }
+        }
+        
+        System.out.println("Heads:"+ omote +",Tails:"+ ura);
+        
+        }  
+    }
